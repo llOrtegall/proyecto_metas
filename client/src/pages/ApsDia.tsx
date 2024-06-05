794ffef578e9af7408f146a68750ac8e17085213
llOrtegall <IvanOrtega_97@hotmail.com>
2024-06-05 17:04:57 -0500

    continuar mañana los otras pages y el hook useFeth Data

--- client/src/pages/ApsDia.tsx
+++ client/src/pages/ApsDia.tsx
@@ -1,9 +1,10 @@
 import { BarraProgressProduct } from '../components/ui/ProgresoProducto'
+import { Loading } from '../components/animations/Loading'
 import { useFecthMetasData } from '../hooks/useFetchData'
-import { MetasProps } from '../types/Metas'
 import { ArrowsIcon } from '../components/icons'
-import { useMemo, useState } from 'react'
 import { sortData } from '../utils/funtions'
+import { MetasProps } from '../types/Metas'
+import { useMemo, useState } from 'react'
 
 function AspDiaPage ({ codigo, zona }: MetasProps) {
   const { data, error, isLoading } = useFecthMetasData('/cumpDiaProd', zona, codigo)
@@ -14,16 +15,27 @@ function AspDiaPage ({ codigo, zona }: MetasProps) {
   }, [data, isAscending])
 
   return (
-    <section>
-      <section className='w-full flex items-center justify-center gap-10'>
+    <section className='relative'>
+      <section className='w-full flex items-center justify-center gap-10 py-4'>
         <h1 className='text-2xl font-semibold'>Aspiración Día Actual</h1>
         <button onClick={() => setIsAscending(!isAscending)} className='flex p-3 items-center justify-center bg-blue-600 text-lg rounded-md gap-4 text-white font-semibold hover:bg-blue-500'>
           <p className='text-center'>Cambiar Orden</p>
           <ArrowsIcon />
         </button>
+
+        {
+          isLoading
+            ? (
+              <div className='absolute z-50 left-8 top-2 flex flex-col items-center justify-center'>
+                <Loading />
+                <p className='text-blue-600 font-semibold'>Cargando y/o Actualizando Información ...</p>
+              </div>
+              )
+            : null
+        }
+
       </section>
       <div className='grid grid-cols-4 gap-2 px-2'>
-        {isLoading ? <p>Cargando...</p> : null}
         {error ? <p>Ocurrió un error al solicitar la data recargue la página o intentelo más tarde</p> : null}
         {
             sortedData.map(meta => (
