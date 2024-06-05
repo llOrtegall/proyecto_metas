60513dda0543652d0450a7746b7cf9a65333835f
llOrtegall <IvanOrtega_97@hotmail.com>
2024-06-05 16:20:29 -0500

    refactor: Add sortData function to useFetchData hook

--- client/src/hooks/useFetchData.ts
+++ client/src/hooks/useFetchData.ts
@@ -2,6 +2,17 @@ import { MetasProducto } from '../types/Metas'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+function sortData (data: MetasProducto[], isAscending: boolean): MetasProducto[] {
+  return [...data].sort((a, b) => {
+    // Siempre coloca el elemento con id 'especial' en primer lugar
+    if (a.id === 17) return -1
+    if (b.id === 17) return 1
+
+    // Para todos los demás elementos, ordena por porcentaje
+    return isAscending ? parseFloat(a.porcentaje) - parseFloat(b.porcentaje) : parseFloat(b.porcentaje) - parseFloat(a.porcentaje)
+  })
+}
+
 export function useFecthMetasData (url: string, company: string, codigo: number) {
   const [data, setData] = useState<MetasProducto[]>([])
   const [error, setError] = useState<Error | null>(null)
@@ -17,17 +28,7 @@ export function useFecthMetasData (url: string, company: string, codigo: number)
   }, [url, company, codigo])
 
   // Verifica que los datos existen antes de intentar ordenarlos
-  const sortedData = Array.isArray(data)
-    ? [...data]
-        .sort((a, b) => {
-        // Siempre coloca el elemento con id 'especial' en primer lugar
-          if (a.id === 17) return -1
-          if (b.id === 17) return 1
-
-          // Para todos los demás elementos, ordena por porcentaje
-          return isAscending ? parseFloat(a.porcentaje) - parseFloat(b.porcentaje) : parseFloat(b.porcentaje) - parseFloat(a.porcentaje)
-        })
-    : []
+  const sortedData = Array.isArray(data) ? sortData(data, isAscending) : []
 
   return { sortedData, isLoading, error, setIsAscending, isAscending }
 }
