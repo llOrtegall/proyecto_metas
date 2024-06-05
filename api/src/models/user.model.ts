4bf1b442b6bacb8499f20ec6020fd1e01e8c7a48
llOrtegall <IvanOrtega_97@hotmail.com>
2024-06-05 10:42:52 -0500

    format two models

--- api/src/models/user.model.ts
+++ api/src/models/user.model.ts
@@ -4,29 +4,11 @@ import { MetasConn } from "../connections/metasConn";
 class User extends Model { }
 
 User.init({
-  codigo: {
-    type: DataTypes.NUMBER,
-    allowNull: false,
-    unique: true
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    primaryKey: true,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  estado: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  nombres: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
+  codigo: { type: DataTypes.NUMBER, allowNull: false, unique: true },
+  username: { type: DataTypes.STRING, allowNull: false, primaryKey: true, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+  estado: { type: DataTypes.STRING, allowNull: false },
+  nombres: { type: DataTypes.STRING, allowNull: false }
 }, {
   sequelize: MetasConn,
   modelName: 'user',
