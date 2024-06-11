f553a1e6c3d7c744b6177fa78690f7574f34c218
llOrtegall <IvanOrtega_97@hotmail.com>
2024-06-11 09:20:47 -0500

    modo dark categories

--- client/src/components/HistoCatComp.tsx
+++ client/src/components/HistoCatComp.tsx
@@ -9,7 +9,7 @@ export function HistComponent ({ data }:{data: Historial[]}) {
       {
       data.map(hist => (
         <ul key={hist.MES} className='flex flex-col w-full items-center gap-2 pt-2'>
-          <p className='w-full text-center font-semibold text-lg'>{meses[hist.MES - 1]}</p>
+          <p className='w-full text-center font-semibold text-lg dark:text-white'>{meses[hist.MES - 1]}</p>
           <figure className='lg:w-20 xl:w-28 2xl:w-32 3xl:w-40'>
             <RenderCategoria cat={hist.CATEGORIA} ver={hist.VERSION} />
           </figure>
